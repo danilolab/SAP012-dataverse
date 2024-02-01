d6a9e9318e4e3bb086c7ba1b815f4fbade72d6fb
Thaysa Borges <borges.thaysa@gmail.com>
2024-02-01 16:56:05 -0300

    datafuncions feita

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,53 @@
-// Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
+export const filterBy = (data, filterValue) => {
 
-export const example = () => {
-  return 'example';
+  const filterString = filterValue;
+  const filteredData = data.filter(item => {
+
+    return item.facts.tempoDeVida === parseInt(filterString) || item.facts.facilidadeCriacao === filterString || item.facts.capturaPresas === filterString;
+  });
+
+  return filteredData;
 };
 
-export const anotherExample = () => {
-  return [];
+export const sortBy = (data, itemSelecionado, order) => {
+  data.sort((a, b) => {
+    if (itemSelecionado === "alturaPlanta" || itemSelecionado === "precoPlanta") {
+
+      return a.facts[itemSelecionado] - b.facts[itemSelecionado];
+    } else if (itemSelecionado === "name") {
+
+      return a[itemSelecionado].localeCompare(b[itemSelecionado]);
+    } else {
+
+      return 0;
+    }
+  });
+
+  if (order === "desc") {
+    data.reverse();
+  }
+
+  return data;
 };
+
+export const calculaMediaVida = (data) => {
+  const soma = data.reduce(
+    function (acumulador, item) {
+      return acumulador + item.facts.tempoDeVida
+    }, 0
+  );
+  const media = soma / data.length;
+  return media.toFixed(0)
+}
+
+export const calculaMediaAltura = (data) => {
+  const soma = data.reduce(
+    function (acumulador, item) {
+      return acumulador + item.facts.alturaPlanta
+    }, 0
+  );
+  const media = soma / data.length;
+  return media.toFixed(0)
+}
+
+export default sortBy
